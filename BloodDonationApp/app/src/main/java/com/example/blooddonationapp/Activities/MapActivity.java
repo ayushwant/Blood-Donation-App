cc42802cea85397e814e89ea8d578d0892c5c5a3
ayushwant <ayushwant912@gmail.com>
2022-02-24 16:18:38 +0530

    created MapActivity

---
+++ BloodDonationApp/app/src/main/java/com/example/blooddonationapp/Activities/MapActivity.java
@@ -0,0 +1,22 @@
+package com.example.blooddonationapp.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.blooddonationapp.Fragments.FeedFragment;
+import com.example.blooddonationapp.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+    }
+
+}
