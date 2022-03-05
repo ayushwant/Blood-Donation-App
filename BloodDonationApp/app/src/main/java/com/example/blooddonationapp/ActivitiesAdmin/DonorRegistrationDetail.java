421892495a999cf62385b7e706f10b58300619c9
Asmita33 <asmitayadav1512000@gmail.com>
2022-03-05 10:17:30 +0530

    notification

--- BloodDonationApp/app/src/main/java/com/example/blooddonationapp/ActivitiesAdmin/DonorRegistrationDetail.java
+++ BloodDonationApp/app/src/main/java/com/example/blooddonationapp/ActivitiesAdmin/DonorRegistrationDetail.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.example.blooddonationapp.MainActivityAdmin;
 import com.example.blooddonationapp.ModelClasses.Donor;
+import com.example.blooddonationapp.ModelClasses.Notification;
 import com.example.blooddonationapp.R;
 import com.example.blooddonationapp.databinding.ActivityDonorRegistrationDetailBinding;
 import com.example.blooddonationapp.databinding.ActivityMainBinding;
@@ -21,6 +22,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -29,6 +32,7 @@ public class DonorRegistrationDetail extends AppCompatActivity {
     ActivityDonorRegistrationDetailBinding binding;
     private FirebaseAuth auth;
     private FirebaseUser currentUser;
+    private DatabaseReference mDatabase;
     private FirebaseFirestore db;
     private String number;
     private boolean isPublic;
@@ -43,6 +47,8 @@ public class DonorRegistrationDetail extends AppCompatActivity {
         auth=FirebaseAuth.getInstance();
         db= FirebaseFirestore.getInstance();
         currentUser=auth.getCurrentUser();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Intent i=getIntent();
         number=i.getStringExtra("phone");
@@ -93,6 +99,14 @@ public class DonorRegistrationDetail extends AppCompatActivity {
                             @Override
                             public void onSuccess(Void unused) {
 
+                                Notification notification=new Notification();
+                                notification.setLine1("Donor Registration");
+                                notification.setLine2("Your request has been verified");
+                                notification.setLine3("");
+                                notification.setSeen(false);
+
+                                mDatabase.child("Notifications")
+                                        .child(number).push().setValue(notification);
 
                             }
                         }).addOnFailureListener(new OnFailureListener() {
@@ -150,6 +164,16 @@ public class DonorRegistrationDetail extends AppCompatActivity {
                                         Intent i= new Intent(DonorRegistrationDetail.this, MainActivityAdmin.class);
                                         startActivity(i);
                                         //Notifying
+
+                                        Notification notification=new Notification();
+                                        notification.setLine1("Blood Donation Verification");
+                                        notification.setLine2("You are not verified to donate");
+                                        notification.setLine3("Kindly check your credentials before applying");
+                                        notification.setSeen(false);
+
+                                        mDatabase.child("Notifications")
+                                                .child(number).push().setValue(notification);
+
 
                                     }
                                 });
